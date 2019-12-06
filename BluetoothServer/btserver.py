29d091009a532cbcd27658b9f5f7effd4dc1d658
danielgray2 <daniel.gray@buildto.io>
2019-12-05 22:59:26 -0700

    Comitted and deleted requirements.txt

--- BluetoothServer/btserver.py
+++ BluetoothServer/btserver.py
@@ -1,9 +1,18 @@
 import bluetooth
-import RPi.GPIO as GPIO
 import pigpio
 import json
 
+'''
+This module serves as a Bluetooth Server. This class
+handles all of the logic on the server side of things.
+'''
+
 class BtServer:
+	'''
+	This is the only class in this file. It sets up all of
+	the control pins, binds to the controller, and handles
+	inputs
+	'''
 	def __init__(self):
 		# Setup pins
 		self.SPEED_PIN = 21
@@ -12,7 +21,6 @@ class BtServer:
 		# Setup commands
 		self.UPDATE_SPEED = "s"
 		self.UPDATE_DIRECTION = "d"
-		self.SHUTDOWN = "shutdown"
 		self.READY_FOR_UPDATE = "rfu"
 
 		# Intialize GPIO
@@ -22,6 +30,13 @@ class BtServer:
 		self.server = self.create_bt_server()
 
 	def create_bt_server(self):
+		'''
+		This method creates the Bluetooth connection
+		and opens a socket.
+
+		Code for this function and the next was found here:
+		https://electronicshobbyists.com/controlling-gpio-through-android-app-over-bluetooth-raspberry-pi-bluetooth-tutorial/
+		'''
 		server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 		host = ""
 		port = 1 # Raspberry Pi uses port 1 for Bluetooth communication
@@ -37,6 +52,9 @@ class BtServer:
 		return server
 		
 	def start_listening(self):
+		'''
+		This method connects to the phone
+		'''
 		# Server accepts the clients request and assigns a mac address. 
 		self.client, address = self.server.accept()
 		print("Connected To", address)
@@ -47,7 +65,6 @@ class BtServer:
 			try:
         		# Receivng the data. 
 				data = self.client.recv(1024).decode("utf-8") # 1024 is the buffer size.
-				print("Here is the data: " + str(data))
 				parsed_data = json.loads(data)
 				if(parsed_data['e'] == self.UPDATE_SPEED):
 					self.update_speed(int(parsed_data['v']))
@@ -69,41 +86,19 @@ class BtServer:
 				self.start_listening()
 
 	def update_speed(self, value):
-		#slope = -5.85
-		#y_intercept = 1455
-		#pwm_value = slope * value + y_intercept
-		#self.pi.set_servo_pulsewidth(self.SPEED_PIN, pwm_value)
+		'''
+		This handles the speed using a linear equation
+		'''
 		slope = 10
 		y_intercept = 1000
 		pwm_value = slope * value + y_intercept
 		self.pi.set_servo_pulsewidth(self.SPEED_PIN, pwm_value)
-		print("Here is the PWM: " + str(pwm_value))
 
 	def update_direction(self, value):
-		#slope = 0.0388
-		#y_intercept = 7
-		#processed_value = abs((value - 50)) % 100
-		#print("Here is the processed value: " + str(processed_value))
-		#if(value > 50):
-		#	processed_value = -processed_value
-		#pwm_value = slope * processed_value + y_intercept
-		##print("Here is the pwm_value: " + str(pwm_value))
-		#self.lateral_pwm.ChangeDutyCycle(pwm_value)
+		'''
+		This handles the speed using a linear equation
+		'''
 		slope = -3.8
 		y_intercept = 2100
 		pwm_value = slope * value + y_intercept
 		self.pi.set_servo_pulsewidth(self.LATERAL_PIN, pwm_value)
-		print("RAW_VALUE: " + str(value))
-		print("PWM_VALUE: " + str(pwm_value))
-	
-	def shutdown(self):
-		# Making all the output pins LOW
-		GPIO.cleanup()
-
-		# Closing the client and server connection
-		self.client.close()
-		self.server.close()
-	
-	def stop_pins(self):
-		for pin in self.pin_array:
-			GPIO.output(pin, False)
--- BluetoothServer/main.py
+++ BluetoothServer/main.py
@@ -1,7 +1,11 @@
 from btserver import BtServer
 import time
 
+'''
+This is the main function that is
+called and runs the code
+'''
+
 if __name__ == "__main__":
-	#time.sleep(15)
 	btServer = BtServer()
 	btServer.start_listening()
